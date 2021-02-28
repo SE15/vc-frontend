77df324446d846c929e0c84edd6f72590166f631
Thushani-Jayasekera <thushanij.18@cse.mrt.ac.lk>
2021-02-28 14:34:21 +0530

    skill unit test (*App.test.js was deleted)

---
+++ src/components/Skills/Skill/Skill.test.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Button,AddIcon,DeleteIcon} from '@chakra-ui/icons';
+
+import {configure,mount,shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer'
+
+import Skill from './Skill'
+
+configure({adapter:new Adapter()});
+
+describe('<Skill />',()=>{
+    let wrapper;
+    beforeEach(()=>{
+        wrapper =shallow(<Skill name='Anne De Mel' validations={5} />);
+    });
+    it('should set isValidated to true when validated',()=>{
+        
+        expect(wrapper.instance().state.isValidated).toBe(false);
+        
+
+        wrapper.instance().handleValidation()
+        expect(wrapper.instance().state.isValidated).toBe(true);
+       
+    });
+
+    it('should increase validations when validated',()=>{
+        
+        expect(wrapper.instance().state.validation).toBe(5);
+        
+
+        wrapper.instance().handleValidation()
+        expect(wrapper.instance().state.validation).toBe(6);
+       
+    });
+    
+    it('should render red colour button if its not a visit by other user',()=>{
+        wrapper.setProps({visit:false})
+        expect(wrapper.getElements()).toMatchSnapshot();
+    });
+    
+    it('should render blue colour button if its a visit by other user',()=>{
+        wrapper.setProps({visit:true})
+        expect(wrapper.getElements()).toMatchSnapshot();
+    });
+
+    it('should increase validations and disable add button when validated',()=>{
+        wrapper.instance().handleValidation();
+        expect(wrapper.getElements()).toMatchSnapshot();
+    });
+
+   
+});
