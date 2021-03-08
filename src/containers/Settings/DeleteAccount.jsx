3625990ed31388a96c017727767bc8399598e922
nipunayf <nipuna.18@cse.mrt.ac.lk>
2021-03-08 05:52:14 +0530

    ADD - delete account functionality

--- src/containers/Settings/DeleteAccount.jsx
+++ src/containers/Settings/DeleteAccount.jsx
@@ -6,10 +6,53 @@ import {
     StackDivider,
     Spacer,
     Alert,
-    AlertIcon
+    AlertIcon,
+    useDisclosure,
+    useToast
 } from '@chakra-ui/react';
 
-const DeleteAccount = () => {
+import { useState } from 'react';
+import PopupWindow from '../../components/ContainerTemplates/PopupWindow';
+
+import * as actions from '../../store/actions';
+
+import { deleteUser } from '../../api';
+import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+const DeleteAccount = ({user, onLogout}) => {
+    const [loading, setLoading] = useState(false);
+    const {isOpen, onOpen, onClose} = useDisclosure();
+    const toast = useToast();
+    const history = useHistory();
+
+    const onDelteAccount = async () => {
+        setLoading(true);
+        const result = await deleteUser(user);
+        if (result.data) {
+            toast({
+                position: "bottom-left",
+                title: `Account deleted successfully`,
+                description: `You have deleted your account`,
+                status: "success",
+                isClosable: true,
+                htmlWidth: 200
+            });
+            onLogout();
+            history.push('/');
+        } else {
+            toast({
+                position: "bottom-left",
+                title: 'Account deleting failed',
+                description: result.message,
+                status: "error",
+                isClosable: true,
+                htmlWidth: 200
+            });
+        }
+        setLoading(false);
+    }
+
     return (
         <VStack py={5} w="100%" h="100%" justify="center">
             <Heading size="md" color="purple.700">
@@ -18,15 +61,40 @@ const DeleteAccount = () => {
             <StackDivider borderColor="purple.100" borderWidth={1} />
             <Spacer />
             <VStack spacing={3}>
-                <Button colorScheme="red" w="40%">Delete Account</Button>
+                <Button colorScheme="red" w="40%" onClick={onOpen} isDisabled={isOpen}>Delete Account</Button>
                 <Alert status="error" w="80%">
                     <AlertIcon />
                     Once you delete your account, you cannot restore it.
                 </Alert>
             </VStack>
-            <Spacer />
+            <PopupWindow
+                title="Delete Account"
+                buttonName="Delete"
+                onClick={onDelteAccount}
+                isLoading={loading}
+                isOpen={isOpen}
+                onClose={onClose}>
+                <Text pb={2}>Do you wish to delete your account?</Text>
+                <Alert status="warning" borderRadius="1rem" h="25%" w="100%" pt={2}>
+                    <AlertIcon />
+                    You cannot undo this action once you delete your account
+                </Alert>
+            </PopupWindow>
         </VStack>
     );
 }
 
-export default DeleteAccount;
+const mapStateToProps = state => {
+    return {
+        user: state.user
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onLogout: () => dispatch(actions.logout('/'))
+    };
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(DeleteAccount);
